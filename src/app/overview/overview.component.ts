138f6ecbcc402d4d9f31a95201e54a961ab15391
Jelte Wiersma <jelte@softwarebastards.nl>
2024-07-19 14:28:09 +0200

    Add cards

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -7,6 +7,7 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { Subject } from 'rxjs/internal/Subject';
 
 @Component({
   selector: 'app-overview',
@@ -27,24 +28,52 @@ export class OverviewComponent {
 
   private breakpointObserver = inject(BreakpointObserver);
 
+  public myObservable = new Subject<any>();
+  
+  private smallViewGrid;
+  private largeViewGrid;
+
+
+  constructor() {
+
+    this.myObservable.subscribe((val) => {
+      this.largeViewGrid = val;
+      console.log('new value:', val);
+    });
+
+    this.largeViewGrid = [
+      { title: 'Card 1', cols: 2, rows: 1 },
+      { title: 'Card 2', cols: 1, rows: 2 },
+      { title: 'Card 3', cols: 1, rows: 1 },
+      { title: 'Card 4', cols: 1, rows: 1 }
+    ];
+
+    this.smallViewGrid = [
+      { title: 'Card 1', cols: 2, rows: 1 },
+      { title: 'Card 2', cols: 2, rows: 1 },
+      { title: 'Card 3', cols: 2, rows: 1 },
+      { title: 'Card 4', cols: 2, rows: 1 }
+    ];
+    
+    setTimeout(() => {
+      this.myObservable.next([
+        { title: 'Card 1', cols: 2, rows: 1 },
+        { title: 'Card 2', cols: 1, rows: 2 },
+        { title: 'Card 3', cols: 1, rows: 2 },
+        { title: 'Card 4', cols: 1, rows: 2 }
+      ]);
+    }, 2000);
+
+  }
+
   /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+  public cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
+        return this.smallViewGrid 
       }
-
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
+      
+      return this.largeViewGrid;
     })
   );
 }
